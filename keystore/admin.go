85dd5624588cdc02fd3168809a092810b1ddf773
donnie <donnie4w@gmail.com>
2023-12-07 11:50:35 +0800

    Create admin.go

---
+++ keystore/admin.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/tim
+//
+
+package keystore
+
+import (
+	. "github.com/donnie4w/gofer/keystore"
+)
+
+var Admin = NewStoreAdmin()
