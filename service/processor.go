909ebbfc61c8690813e7fa58d01afea611ba6e2c
donnie4w <donnie4w@gmail.com>
2024-01-21 16:49:15 +0800

    Update processor.go

--- service/processor.go
+++ service/processor.go
@@ -7,6 +7,7 @@
 package service
 
 import (
+	. "github.com/donnie4w/gofer/buffer"
 	"github.com/donnie4w/tim/data"
 	. "github.com/donnie4w/tim/stub"
 	"github.com/donnie4w/tim/sys"
@@ -30,15 +31,23 @@ func timMessage(tm *TimMessage, transType int8) (err sys.ERROR) {
 			ol := false
 			node := tm.ToTid.Node
 			if wsware.hasUser(node) {
-				if tm.OdType != sys.ORDER_STREAM {
+				if tm.OdType != sys.ORDER_STREAM && tm.OdType != sys.ORDER_BIGSTRING && tm.OdType != sys.ORDER_BIGBINARY {
 					if wsware.SendNodeWithAck(node, tm, sys.TIMMESSAGE) {
 						ol = true
 					}
+				} else if tm.OdType == sys.ORDER_BIGSTRING {
+					wsware.SendBigData(tm.ToTid.Node, []byte(tm.FromTid.Node+sys.SEP_STR+tm.GetDataString()), sys.TIMBIGSTRING)
+				} else if tm.OdType == sys.ORDER_BIGBINARY {
+					buf := NewBuffer()
+					buf.WriteString(tm.FromTid.Node)
+					buf.WriteByte(sys.SEP_BIN)
+					buf.Write(tm.GetDataBinary())
+					wsware.SendBigData(tm.ToTid.Node, buf.Bytes(), sys.TIMBIGBINARY)
 				} else {
 					wsware.SendNode(node, tm, sys.TIMMESSAGE)
 				}
 			}
-			if !ol && tm.OdType != sys.ORDER_STREAM {
+			if !ol && tm.OdType != sys.ORDER_STREAM && tm.OdType != sys.ORDER_BIGSTRING && tm.OdType != sys.ORDER_BIGBINARY {
 				data.Handler.SaveOfflineMessage(tm)
 			}
 		}
@@ -75,9 +84,17 @@ func timStream(vb *VBean) (err sys.ERROR) {
 	defer sys.Stat.TxDone()
 	if vr, ok := vgate.VGate.GetVroom(vb.Vnode); ok {
 		vr.Updatetime()
-		vr.Nodes().Range(func(wsId int64, _ int8) bool {
-			ts := &TimStream{ID: *vb.StreamId, VNode: vb.Vnode, FromNode: *vb.Rnode, Body: vb.Body, Dtype: vb.Dtype}
-			util.GoPoolTx2.Go(func() { wsware.SendWs(wsId, ts, sys.TIMSTREAM) })
+		vr.Nodes().Range(func(wsId int64, ib int8) bool {
+			if ib == 0 {
+				ts := &TimStream{ID: *vb.StreamId, VNode: vb.Vnode, FromNode: *vb.Rnode, Body: vb.Body, Dtype: vb.Dtype}
+				util.GoPoolTx2.Go(func() { wsware.SendWs(wsId, ts, sys.TIMSTREAM) })
+			} else if ib == 1 {
+				buf := NewBuffer()
+				buf.WriteString(vb.Vnode)
+				buf.WriteByte(sys.SEP_BIN)
+				buf.Write(vb.Body)
+				util.GoPoolTx2.Go(func() { wsware.SendBigDataByWs(wsId, buf.Bytes(), sys.TIMBIGBINARYSTREAM)})
+			}
 			return true
 		})
 	} else {
