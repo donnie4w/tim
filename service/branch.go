09471f9d1cbb7966a9212cafa3c09278aedbc7da
donnie4w <donnie4w@gmail.com>
2024-08-22 14:18:59 +0800

    Update branch.go

--- service/branch.go
+++ service/branch.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2023, donnie <donnie4w@gmail.com>
 // All rights reserved.
-// Use of t source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 //
 // github.com/donnie4w/tim
