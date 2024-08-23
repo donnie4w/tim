efa225203d584baf5b667a7ef903d86f2d1e25bc
donnie4w <donnie4w@gmail.com>
2024-08-23 10:40:53 +0800

    Update cache_test.go

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -8,32 +8,27 @@
 
 package cache
 
-import (
-	"fmt"
-	"testing"
-)
-
-func Test_cache(t *testing.T) {
-	chatIdCache := NewLruPool[int, any](32, 100)
-
-	for i := 0; i < 1<<10; i++ {
-		chatIdCache.Put(i, nil)
-	}
-
-	for i := 0; i < 1<<10; i++ {
-		if _, ok := chatIdCache.Get(i); !ok {
-			fmt.Println("err>>", i)
-		}
-	}
-}
-
-func BenchmarkParallel_cache(b *testing.B) {
-	chatIdCache := NewLruPool[int, any](32, 100)
-	b.RunParallel(func(pb *testing.PB) {
-		i := 0
-		for pb.Next() {
-			i++
-			chatIdCache.Put(i, nil)
-		}
-	})
-}
+//func Test_cache(t *testing.T) {
+//	chatIdCache := NewLruPool[int, any](32, 100)
+//
+//	for i := 0; i < 1<<10; i++ {
+//		chatIdCache.Put(i, nil)
+//	}
+//
+//	for i := 0; i < 1<<10; i++ {
+//		if _, ok := chatIdCache.Get(i); !ok {
+//			fmt.Println("err>>", i)
+//		}
+//	}
+//}
+//
+//func BenchmarkParallel_cache(b *testing.B) {
+//	chatIdCache := NewLruPool[int, any](32, 100)
+//	b.RunParallel(func(pb *testing.PB) {
+//		i := 0
+//		for pb.Next() {
+//			i++
+//			chatIdCache.Put(i, nil)
+//		}
+//	})
+//}
