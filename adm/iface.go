e9ee9395dbb60b6d792a62e6ce683d15354224c0
donnie4w <donnie4w@gmail.com>
2025-01-03 23:22:06 +0800

    Update iface.go

--- adm/iface.go
+++ adm/iface.go
@@ -4,44 +4,41 @@
 // license that can be found in the LICENSE file.
 //
 // github.com/donnie4w/tim
+
 package adm
 
 import (
 	"context"
+	"github.com/donnie4w/tim/errs"
 	. "github.com/donnie4w/tim/stub"
 	"github.com/donnie4w/tim/util"
 )
 
-type ifacehandle struct {
-}
+type ifacehandle struct{}
 
 var ifaceProcessor = &ifacehandle{}
 
-// Parameters:
-//   - Loginname
-//   - Domain
-//   - Pwd
-func (t *ifacehandle) ResetAuth(ctx context.Context, ab *AuthBean) (_r *AdmAck, _err error) {
-	defer util.Recover()
+func (t *ifacehandle) ModifyPwd(ctx context.Context, fromnode, oldpwd, newpwd, domain string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if e := noAuthAndClose(cc); e != nil {
 		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
 		return
 	}
-	if ab.Username != nil || ab.Password != nil {
+	if fromnode == "" || newpwd == "" {
 		_r = newAdmAck(false)
+		_r.Errcode = errs.ERR_PARAMS.TimError().Code
 		return
 	}
-	_r = Admhandler.ResetAuth(ab)
+	_r = Admhandler.ModifyPwd(fromnode, oldpwd, newpwd, domain)
 	return
 }
 
-// Parameters:
-//   - Ab
 func (t *ifacehandle) Auth(ctx context.Context, ab *AuthBean) (_r *AdmAck, _err error) {
-	defer util.Recover()
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
@@ -52,7 +49,7 @@ func (t *ifacehandle) Auth(ctx context.Context, ab *AuthBean) (_r *AdmAck, _err 
 }
 
 func (t *ifacehandle) Ping(ctx context.Context) (_r *AdmAck, _err error) {
-	defer util.Recover()
+	defer util.Recover2(&_err)
 	mux := ctx2CliContext(ctx).mux
 	defer mux.Unlock()
 	mux.Lock()
@@ -60,183 +57,518 @@ func (t *ifacehandle) Ping(ctx context.Context) (_r *AdmAck, _err error) {
 	return
 }
 
-// Parameters:
-//   - Atoken
 func (t *ifacehandle) Token(ctx context.Context, atoken *AdmToken) (_r *AdmAck, _err error) {
-	defer util.Recover()
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if e := noAuthAndClose(cc); e != nil {
 		_r = newAdmAck(false)
-		return
-	}
-	if atoken.GetName() == "" {
-		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
 		return
 	}
 	_r = Admhandler.Token(atoken)
 	return
 }
 
-// Parameters:
-//   - Am
-func (t *ifacehandle) OsMessage(ctx context.Context, am *AdmMessage) (_r *AdmAck, _err error) {
-	defer util.Recover()
+func (t *ifacehandle) TimMessageBroadcast(ctx context.Context, amb *AdmMessageBroadcast) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if e := noAuthAndClose(cc); e != nil {
 		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
 		return
 	}
-	_r = Admhandler.OsMessage(am)
+	_r = Admhandler.TimMessageBroadcast(amb)
 	return
 }
 
-// Parameters:
-//   - amb
-func (t *ifacehandle) ProxyMessage(ctx context.Context, amb *AdmProxyMessage) (_r *AdmAck, _err error) {
-	defer util.Recover()
-
+func (t *ifacehandle) TimPresenceBroadcast(ctx context.Context, apb *AdmPresenceBroadcast) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if amb.GetConnectid() <= 0 || noAuthAndClose(cc) {
+	if e := noAuthAndClose(cc); e != nil {
 		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.TimPresenceBroadcast(apb)
+	return
+}
+
+func (t *ifacehandle) ProxyMessage(ctx context.Context, amb *AdmProxyMessage) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
 		return
 	}
 	_r = Admhandler.ProxyMessage(amb)
 	return
 }
 
-// Parameters:
-//   - ab
 func (t *ifacehandle) Register(ctx context.Context, ab *AuthBean) (_r *AdmAck, _err error) {
-	defer util.Recover()
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if e := noAuthAndClose(cc); e != nil {
 		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
 		return
 	}
 	_r = Admhandler.Register(ab)
 	return
 }
 
-// Parameters:
-//   - Amui
 func (t *ifacehandle) ModifyUserInfo(ctx context.Context, amui *AdmModifyUserInfo) (_r *AdmAck, _err error) {
-	defer util.Recover()
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if e := noAuthAndClose(cc); e != nil {
 		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
 		return
 	}
 	_r = Admhandler.ModifyUserInfo(amui)
 	return
 }
 
-// Parameters:
-//   - Abu
-func (t *ifacehandle) BlockUser(ctx context.Context, abu *AdmBlockUser) (_r *AdmAck, _err error) {
-	defer util.Recover()
+func (t *ifacehandle) SysBlockUser(ctx context.Context, abu *AdmSysBlockUser) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if e := noAuthAndClose(cc); e != nil {
 		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
 		return
 	}
-	_r = Admhandler.BlockUser(abu)
+	_r = Admhandler.SysBlockUser(abu)
 	return
 }
 
-func (t *ifacehandle) BlockList(ctx context.Context) (_r *AdmBlockList, _err error) {
-	defer util.Recover()
+func (t *ifacehandle) SysBlockList(ctx context.Context) (_r *AdmSysBlockList, _err error) {
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if noAuthAndClose(cc) != nil {
 		return
 	}
-	_r = Admhandler.BlockList()
+	_r = Admhandler.SysBlockList()
 	return
 }
 
-// Parameters:
-//   - Au
 func (t *ifacehandle) OnlineUser(ctx context.Context, au *AdmOnlineUser) (_r *AdmTidList, _err error) {
-	defer util.Recover()
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if noAuthAndClose(cc) != nil {
 		return
 	}
 	_r = Admhandler.OnlineUser(au)
 	return
 }
 
-// Parameters:
-//   - Avb
 func (t *ifacehandle) Vroom(ctx context.Context, avb *AdmVroomBean) (_r *AdmAck, _err error) {
-	defer util.Recover()
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if e := noAuthAndClose(cc); e != nil {
 		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
 		return
 	}
 	_r = Admhandler.Vroom(avb)
 	return
 }
 
-// Parameters:
-//   - Arb
-func (t *ifacehandle) TimRoom(ctx context.Context, arb *AdmRoomReq) (_r *AdmAck, _err error) {
-	defer util.Recover()
-	cc := ctx2CliContext(ctx)
-	cc.mux.Lock()
-	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
-		_r = newAdmAck(false)
-		return
-	}
-	_r = Admhandler.TimRoom(arb)
-	return
-}
-
-// Parameters:
-//   - Adb
 func (t *ifacehandle) Detect(ctx context.Context, adb *AdmDetectBean) (_r *AdmAck, _err error) {
-	defer util.Recover()
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if e := noAuthAndClose(cc); e != nil {
 		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
 		return
 	}
 	_r = Admhandler.Detect(adb)
 	return
 }
 
-// Parameters:
-//   - Arb
 func (t *ifacehandle) ModifyRoomInfo(ctx context.Context, arb *AdmRoomBean) (_r *AdmAck, _err error) {
-	defer util.Recover()
+	defer util.Recover2(&_err)
 	cc := ctx2CliContext(ctx)
 	cc.mux.Lock()
 	defer cc.mux.Unlock()
-	if noAuthAndClose(cc) {
+	if e := noAuthAndClose(cc); e != nil {
 		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
 		return
 	}
 	_r = Admhandler.ModifyRoomInfo(arb)
+	return
+}
+
+func (t *ifacehandle) Roster(ctx context.Context, fromnode, domain string) (_r *AdmNodeList, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e == nil {
+		_r = Admhandler.Roster(fromnode, domain)
+	}
+	return
+}
+
+func (t *ifacehandle) Addroster(ctx context.Context, fromnode, domain string, tonode string, msg string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.Addroster(fromnode, domain, tonode, msg)
+	return
+}
+
+func (t *ifacehandle) Rmroster(ctx context.Context, fromnode, domain string, tonode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.Rmroster(fromnode, domain, tonode)
+	return
+}
+
+func (t *ifacehandle) Blockroster(ctx context.Context, fromnode, domain string, tonode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.Blockroster(fromnode, domain, tonode)
+	return
+}
+
+func (t *ifacehandle) PullUserMessage(ctx context.Context, fromnode, domain string, tonode string, mid int64, limit int64) (_r *AdmMessageList, _err error) {
+	return
+}
+
+func (t *ifacehandle) PullRoomMessage(ctx context.Context, fromnode, domain string, tonode string, mid int64, limit int64) (_r *AdmMessageList, _err error) {
+	return
+}
+
+func (t *ifacehandle) OfflineMsg(ctx context.Context, fromnode, domain string, limit int64) (_r *AdmMessageList, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		return
+	}
+	_r = Admhandler.OfflineMsg(fromnode, domain, limit)
+	return
+}
+
+func (ah *ifacehandle) DelOfflineMsg(ctx context.Context, fromnode string, domain string, ids []int64) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		return
+	}
+	_r = Admhandler.DelOfflineMsg(fromnode, domain, ids)
+	return
+}
+
+func (t *ifacehandle) UserRoom(ctx context.Context, fromnode, domain string) (_r *AdmNodeList, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		return
+	}
+	_r = Admhandler.UserRoom(fromnode, domain)
+	return
+}
+
+func (t *ifacehandle) RoomUsers(ctx context.Context, fromnode, domain string) (_r *AdmNodeList, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		return
+	}
+	_r = Admhandler.RoomUsers(fromnode, domain)
+	return
+}
+
+func (t *ifacehandle) CreateRoom(ctx context.Context, fromnode, domain string, topic string, gtype int8) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.CreateRoom(fromnode, domain, topic, gtype)
+	return
+}
+
+func (t *ifacehandle) AddRoom(ctx context.Context, fromnode, domain string, roomNode string, msg string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.AddRoom(fromnode, domain, roomNode, msg)
+	return
+}
+
+func (t *ifacehandle) PullInRoom(ctx context.Context, fromnode, domain string, roomNode string, toNode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.PullInRoom(fromnode, domain, roomNode, toNode)
+	return
+}
+
+func (t *ifacehandle) RejectRoom(ctx context.Context, fromnode, domain string, roomNode string, toNode string, msg string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.RejectRoom(fromnode, domain, roomNode, toNode, msg)
+	return
+}
+
+func (t *ifacehandle) KickRoom(ctx context.Context, fromnode, domain string, roomNode string, toNode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.KickRoom(fromnode, domain, roomNode, toNode)
+	return
+}
+
+func (t *ifacehandle) LeaveRoom(ctx context.Context, fromnode, domain string, roomNode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.LeaveRoom(fromnode, domain, roomNode)
+	return
+}
+
+func (t *ifacehandle) CancelRoom(ctx context.Context, fromnode, domain string, roomNode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.CancelRoom(fromnode, domain, roomNode)
+	return
+}
+
+func (t *ifacehandle) BlockRoom(ctx context.Context, fromnode, domain string, roomNode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.BlockRoom(fromnode, domain, roomNode)
+	return
+}
+
+func (t *ifacehandle) BlockRoomMember(ctx context.Context, fromnode, domain string, roomNode string, toNode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.BlockRoomMember(fromnode, domain, roomNode, toNode)
+	return
+}
+
+func (t *ifacehandle) BlockRosterList(ctx context.Context, fromnode, domain string) (_r *AdmNodeList, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e == nil {
+		_r = Admhandler.BlockRosterList(fromnode, domain)
+	}
+	return
+}
+
+func (t *ifacehandle) BlockRoomList(ctx context.Context, fromnode, domain string) (_r *AdmNodeList, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e == nil {
+		_r = Admhandler.BlockRoomList(fromnode, domain)
+	}
+	return
+}
+
+func (t *ifacehandle) BlockRoomMemberlist(ctx context.Context, fromnode, domain string, roomNode string) (_r *AdmNodeList, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e == nil {
+		_r = Admhandler.BlockRoomMemberlist(fromnode, domain, roomNode)
+	}
+	return
+}
+
+func (t *ifacehandle) VirtualroomRegister(ctx context.Context, fromnode, domain string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.VirtualroomRegister(fromnode, domain)
+	return
+}
+
+func (t *ifacehandle) VirtualroomRemove(ctx context.Context, fromnode, domain string, roomNode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.VirtualroomRemove(fromnode, domain, roomNode)
+	return
+}
+
+func (t *ifacehandle) VirtualroomAddAuth(ctx context.Context, fromnode, domain string, roomNode string, toNode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.VirtualroomAddAuth(fromnode, domain, roomNode, toNode)
+	return
+}
+
+func (t *ifacehandle) VirtualroomDelAuth(ctx context.Context, fromnode, domain string, roomNode string, toNode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.VirtualroomDelAuth(fromnode, domain, roomNode, toNode)
+	return
+}
+
+func (t *ifacehandle) VirtualroomSub(ctx context.Context, wsid int64, fromnode string, domain string, vNode string, subType int8) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.VirtualroomSub(wsid, fromnode, domain, vNode, subType)
+	return
+}
+
+func (t *ifacehandle) VirtualroomUnSub(ctx context.Context, wsid int64, fromnode string, domain string, vNode string) (_r *AdmAck, _err error) {
+	defer util.Recover2(&_err)
+	cc := ctx2CliContext(ctx)
+	cc.mux.Lock()
+	defer cc.mux.Unlock()
+	if e := noAuthAndClose(cc); e != nil {
+		_r = newAdmAck(false)
+		_r.Errcode = e.TimError().Code
+		return
+	}
+	_r = Admhandler.VirtualroomUnSub(wsid, fromnode, domain, vNode)
 	return
 }
