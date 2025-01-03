32ad08631361e0cbe384551d42893e90b4957fc0
donnie4w <donnie4w@gmail.com>
2025-01-03 23:22:52 +0800

    Update cache.go

--- cache/cache.go
+++ cache/cache.go
@@ -4,36 +4,17 @@
 // license that can be found in the LICENSE file.
 //
 // github.com/donnie4w/tim
+
 package cache
 
-//import (
-//	. "github.com/donnie4w/gofer/cache"
-//)
-//
-//type LruPool[K int | int64 | uint64, V any] struct {
-//	lruList []*LruCache[V]
-//	router  []int
-//}
-//
-//func NewLruPool[K int | int64 | uint64, V any](poolsize int, LruLimit int) *LruPool[K, V] {
-//	p := &LruPool[K, V]{}
-//	p.lruList = make([]*LruCache[V], poolsize)
-//	p.router = make([]int, poolsize)
-//	for i := 0; i < poolsize; i++ {
-//		p.lruList[i] = NewLruCache[V](LruLimit)
-//		p.router[i] = i
-//	}
-//	return p
-//}
-//
-//func (this *LruPool[K, V]) Put(t K, v V) {
-//	this.lruList[int(t)%len(this.lruList)].Add(t, v)
-//}
-//
-//func (this *LruPool[K, V]) Get(t K) (v V, b bool) {
-//	return this.lruList[int(t)%len(this.lruList)].Get(t)
-//}
-//
-//func (this *LruPool[K, V]) Del(t K) {
-//	this.lruList[int(t)%len(this.lruList)].Remove(t)
-//}
+import (
+	"github.com/donnie4w/gofer/cache"
+	"github.com/donnie4w/gofer/hashmap"
+	"github.com/donnie4w/tim/stub"
+)
+
+var TokenCache = hashmap.NewLimitHashMap[int64, *stub.Tid](1 << 13)
+
+var AuthCache = hashmap.NewLimitHashMap[uint64, int64](1 << 14)
+
+var AmrCache = cache.NewLruCache[uint64](1 << 20)
