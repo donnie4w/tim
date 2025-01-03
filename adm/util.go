b6306490b9d1d2d5b4ffb73c06f85138848305dc
donnie4w <donnie4w@gmail.com>
2025-01-03 23:22:23 +0800

    Update util.go

--- adm/util.go
+++ adm/util.go
@@ -4,34 +4,40 @@
 // license that can be found in the LICENSE file.
 //
 // github.com/donnie4w/tim
+
 package adm
 
 import (
 	"context"
+	"github.com/donnie4w/tim/data"
+	"github.com/donnie4w/tim/errs"
 	"github.com/donnie4w/tim/stub"
+	"github.com/donnie4w/tim/sys"
 	"strings"
 
 	goutil "github.com/donnie4w/gofer/util"
-	. "github.com/donnie4w/tim/keystore"
+	"github.com/donnie4w/tim/keystore"
 )
 
 func ctx2CliContext(ctx context.Context) *pcontext {
 	return ctx.Value("CliContext").(*pcontext)
 }
 
-func auth(name, pwd string) (b bool) {
-	if _r, ok := Admin.GetAdmin(name); ok {
+func auth(name, pwd, domain string) (b bool) {
+	if sys.Conf.TimAdminAuth {
+		return data.Service.TimAdminAuth(name, pwd, domain)
+	} else if _r, ok := keystore.Admin.GetAdmin(name); ok {
 		b = strings.EqualFold(_r.Pwd, goutil.Md5Str(pwd))
 	}
 	return
 }
 
-func noAuthAndClose(cc *pcontext) (b bool) {
+func noAuthAndClose(cc *pcontext) (err errs.ERROR) {
 	if !cc.isAuth {
 		cc.tt.Close()
-		b = true
+		return errs.ERR_PERM_DENIED
 	}
-	return
+	return nil
 }
 
 func newAdmAck(ok bool) *stub.AdmAck {
@@ -45,7 +51,7 @@ func nodeToTid(node *string) (_r *stub.Tid) {
 	return
 }
 
-func admMessageToTimMessage(amb *stub.AdmMessageBean) *stub.TimMessage {
+func admMessageToTimMessage(amb *stub.AdmMessage) *stub.TimMessage {
 	tm := stub.NewTimMessage()
 	tm.MsType = amb.MsType
 	tm.OdType = amb.OdType
@@ -62,6 +68,53 @@ func admMessageToTimMessage(amb *stub.AdmMessageBean) *stub.TimMessage {
 	tm.Extend = amb.Extend
 	tm.Extra = amb.Extra
 	return tm
+}
+
+func timMessageToAdmMessage(tm *stub.TimMessage) *stub.AdmMessage {
+	am := stub.NewAdmMessage()
+	am.MsType = tm.MsType
+	am.OdType = tm.OdType
+	am.ID = tm.ID
+	am.Mid = tm.Mid
+	am.BnType = tm.BnType
+
+	if tm.FromTid != nil {
+		node := tm.FromTid.Node
+		am.FromNode = &node
+	}
+
+	if tm.ToTid != nil {
+		node := tm.ToTid.Node
+		am.ToNode = &node
+	}
+
+	if tm.RoomTid != nil {
+		node := tm.RoomTid.Node
+		am.RoomNode = &node
+	}
+
+	am.DataBinary = tm.DataBinary
+	am.DataString = tm.DataString
+	am.Udtype = tm.Udtype
+	am.Udshow = tm.Udshow
+	am.Extend = tm.Extend
+	am.Extra = tm.Extra
+	return am
+}
+
+func admPresenceToTimPresence(amp *stub.AdmPresence) *stub.TimPresence {
+	tp := stub.NewTimPresence()
+	tp.ID = amp.ID
+	tp.FromTid = nodeToTid(amp.FromNode)
+	tp.ToTid = nodeToTid(amp.ToNode)
+	tp.ToList = amp.ToList
+	tp.Offline = amp.Offline
+	tp.Status = amp.Status
+	tp.Show = amp.Show
+	tp.Extra = amp.Extra
+	tp.Extend = amp.Extend
+	tp.Extra = amp.Extra
+	return tp
 }
 
 func admUserBeanToTimUserBean(aub *stub.AdmUserBean) *stub.TimUserBean {
@@ -102,4 +155,23 @@ func admTimRoomToTimRoom(atr *stub.AdmTimRoom) *stub.TimRoomBean {
 	rb.Extend = atr.Extend
 	rb.Extra = atr.Extra
 	return rb
+}
+
+func timAckToAdmAck(ta *stub.TimAck) *stub.AdmAck {
+	if ta != nil {
+		aa := stub.NewAdmAck()
+		aa.Ok = &ta.Ok
+		aa.T = ta.T
+		aa.T2 = ta.T2
+		aa.N = ta.N
+		aa.N2 = ta.N2
+		if ta.TimType != 0 {
+			aa.TimType = &ta.TimType
+		}
+		if ta.Error != nil {
+			aa.Errcode = ta.Error.Code
+		}
+		return aa
+	}
+	return nil
 }
