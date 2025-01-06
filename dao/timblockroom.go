58842dc4b37a08172a2f9399097ab82d93592096
donnie4w <donnie4w@gmail.com>
2025-01-06 11:53:11 +0800

    Update timblockroom.go

--- dao/timblockroom.go
+++ dao/timblockroom.go
@@ -5,8 +5,8 @@
 //
 // github.com/donnie4w/gdao
 //
-// datetime :2024-08-10 01:01:22
-// gdao version 1.1.0
+// datetime :2024-10-06 20:30:14
+// gdao version 1.2.0
 // dbtype:sqlite ,database:timdb ,tablename:timblockroom
 
 package dao
@@ -18,120 +18,89 @@ import (
 	
 )
 
-type timblockroom_Id[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue *int64
-}
-
-func (t *timblockroom_Id[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *timblockroom_Id[T]) Value() any {
-	return t.fieldValue
-}
-
-type timblockroom_Unikid[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue *int64
-}
-
-func (t *timblockroom_Unikid[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *timblockroom_Unikid[T]) Value() any {
-	return t.fieldValue
-}
-
-type timblockroom_Uuid[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue *int64
-}
-
-func (t *timblockroom_Uuid[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *timblockroom_Uuid[T]) Value() any {
-	return t.fieldValue
-}
-
-type timblockroom_Tuuid[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue *int64
-}
-
-func (t *timblockroom_Tuuid[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *timblockroom_Tuuid[T]) Value() any {
-	return t.fieldValue
-}
-
 type Timblockroom struct {
 	gdao.Table[Timblockroom]
 
-	Id		*timblockroom_Id[Timblockroom]
-	Unikid		*timblockroom_Unikid[Timblockroom]
-	Uuid		*timblockroom_Uuid[Timblockroom]
-	Tuuid		*timblockroom_Tuuid[Timblockroom]
+	ID      *base.Field[Timblockroom]
+	UNIKID      *base.Field[Timblockroom]
+	UUID      *base.Field[Timblockroom]
+	TUUID      *base.Field[Timblockroom]
+	TIMESERIES      *base.Field[Timblockroom]
+	_ID      *int64
+	_UNIKID      *int64
+	_UUID      *int64
+	_TUUID      *int64
+	_TIMESERIES      *int64
 }
 
+var _Timblockroom_ID = &base.Field[Timblockroom]{"id"}
+var _Timblockroom_UNIKID = &base.Field[Timblockroom]{"unikid"}
+var _Timblockroom_UUID = &base.Field[Timblockroom]{"uuid"}
+var _Timblockroom_TUUID = &base.Field[Timblockroom]{"tuuid"}
+var _Timblockroom_TIMESERIES = &base.Field[Timblockroom]{"timeseries"}
+
 func (u *Timblockroom) GetId() (_r int64){
-	if u.Id.fieldValue != nil {
-		_r = *u.Id.fieldValue
+	if u._ID != nil {
+		_r = *u._ID
 	}
 	return
 }
 
 func (u *Timblockroom) SetId(arg int64) *Timblockroom{
-	u.Put0(u.Id.fieldName, arg)
-	u.Id.fieldValue = &arg
+	u.Put0(u.ID.FieldName, arg)
+	u._ID = &arg
 	return u
 }
 
 func (u *Timblockroom) GetUnikid() (_r int64){
-	if u.Unikid.fieldValue != nil {
-		_r = *u.Unikid.fieldValue
+	if u._UNIKID != nil {
+		_r = *u._UNIKID
 	}
 	return
 }
 
 func (u *Timblockroom) SetUnikid(arg int64) *Timblockroom{
-	u.Put0(u.Unikid.fieldName, arg)
-	u.Unikid.fieldValue = &arg
+	u.Put0(u.UNIKID.FieldName, arg)
+	u._UNIKID = &arg
 	return u
 }
 
 func (u *Timblockroom) GetUuid() (_r int64){
-	if u.Uuid.fieldValue != nil {
-		_r = *u.Uuid.fieldValue
+	if u._UUID != nil {
+		_r = *u._UUID
 	}
 	return
 }
 
 func (u *Timblockroom) SetUuid(arg int64) *Timblockroom{
-	u.Put0(u.Uuid.fieldName, arg)
-	u.Uuid.fieldValue = &arg
+	u.Put0(u.UUID.FieldName, arg)
+	u._UUID = &arg
 	return u
 }
 
 func (u *Timblockroom) GetTuuid() (_r int64){
-	if u.Tuuid.fieldValue != nil {
-		_r = *u.Tuuid.fieldValue
+	if u._TUUID != nil {
+		_r = *u._TUUID
 	}
 	return
 }
 
 func (u *Timblockroom) SetTuuid(arg int64) *Timblockroom{
-	u.Put0(u.Tuuid.fieldName, arg)
-	u.Tuuid.fieldValue = &arg
+	u.Put0(u.TUUID.FieldName, arg)
+	u._TUUID = &arg
+	return u
+}
+
+func (u *Timblockroom) GetTimeseries() (_r int64){
+	if u._TIMESERIES != nil {
+		_r = *u._TIMESERIES
+	}
+	return
+}
+
+func (u *Timblockroom) SetTimeseries(arg int64) *Timblockroom{
+	u.Put0(u.TIMESERIES.FieldName, arg)
+	u._TIMESERIES = &arg
 	return u
 }
 
@@ -146,12 +115,13 @@ func (u *Timblockroom) Scan(fieldname string, value any) {
 		u.SetUuid(base.AsInt64(value))
 	case "tuuid":
 		u.SetTuuid(base.AsInt64(value))
+	case "timeseries":
+		u.SetTimeseries(base.AsInt64(value))
 	}
 }
 
 func (t *Timblockroom) ToGdao() {
-	_t := NewTimblockroom()
-	*t = *_t
+	t.init("timblockroom")
 }
 
 func (t *Timblockroom) Copy(h *Timblockroom) *Timblockroom{
@@ -159,33 +129,30 @@ func (t *Timblockroom) Copy(h *Timblockroom) *Timblockroom{
 	t.SetUnikid(h.GetUnikid())
 	t.SetUuid(h.GetUuid())
 	t.SetTuuid(h.GetTuuid())
+	t.SetTimeseries(h.GetTimeseries())
 	return t
 }
 
 func (t *Timblockroom) String() string {
-	return fmt.Sprint("Id:",t.GetId(), ",","Unikid:",t.GetUnikid(), ",","Uuid:",t.GetUuid(), ",","Tuuid:",t.GetTuuid())
+	return fmt.Sprint("Id:",t.GetId(), ",","Unikid:",t.GetUnikid(), ",","Uuid:",t.GetUuid(), ",","Tuuid:",t.GetTuuid(), ",","Timeseries:",t.GetTimeseries())
+}
+
+func (t *Timblockroom)init(tablename string) {
+	t.ID = _Timblockroom_ID
+	t.UNIKID = _Timblockroom_UNIKID
+	t.UUID = _Timblockroom_UUID
+	t.TUUID = _Timblockroom_TUUID
+	t.TIMESERIES = _Timblockroom_TIMESERIES
+	t.Init(tablename, []base.Column[Timblockroom]{t.ID,t.UNIKID,t.UUID,t.TUUID,t.TIMESERIES})
 }
 
 func NewTimblockroom(tablename ...string) (_r *Timblockroom) {
-
-	id := &timblockroom_Id[Timblockroom]{fieldName: "id"}
-	id.Field.FieldName = "id"
-
-	unikid := &timblockroom_Unikid[Timblockroom]{fieldName: "unikid"}
-	unikid.Field.FieldName = "unikid"
-
-	uuid := &timblockroom_Uuid[Timblockroom]{fieldName: "uuid"}
-	uuid.Field.FieldName = "uuid"
-
-	tuuid := &timblockroom_Tuuid[Timblockroom]{fieldName: "tuuid"}
-	tuuid.Field.FieldName = "tuuid"
-
-	_r = &Timblockroom{Id:id,Unikid:unikid,Uuid:uuid,Tuuid:tuuid}
+	_r = &Timblockroom{}
 	s := "timblockroom"
 	if len(tablename) > 0 && tablename[0] != "" {
 		s = tablename[0]
 	}
-	_r.Init(s, []base.Column[Timblockroom]{id,unikid,uuid,tuuid})
+	_r.init(s)
 	return
 }
 
@@ -195,6 +162,7 @@ func (t *Timblockroom) Encode() ([]byte, error) {
 	m["unikid"] = t.GetUnikid()
 	m["uuid"] = t.GetUuid()
 	m["tuuid"] = t.GetTuuid()
+	m["timeseries"] = t.GetTimeseries()
 	return t.Table.Encode(m)
 }
 
