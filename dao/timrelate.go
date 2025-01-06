b2297ce655870fa0565c027a0afaf83a252a3a12
donnie4w <donnie4w@gmail.com>
2025-01-06 11:53:28 +0800

    Update timrelate.go

--- dao/timrelate.go
+++ dao/timrelate.go
@@ -5,8 +5,8 @@
 //
 // github.com/donnie4w/gdao
 //
-// datetime :2024-08-10 01:01:22
-// gdao version 1.1.0
+// datetime :2024-10-06 20:30:14
+// gdao version 1.2.0
 // dbtype:sqlite ,database:timdb ,tablename:timrelate
 
 package dao
@@ -18,92 +18,73 @@ import (
 	
 )
 
-type timrelate_Id[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue *int64
-}
-
-func (t *timrelate_Id[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *timrelate_Id[T]) Value() any {
-	return t.fieldValue
-}
-
-type timrelate_Uuid[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue *int64
-}
-
-func (t *timrelate_Uuid[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *timrelate_Uuid[T]) Value() any {
-	return t.fieldValue
-}
-
-type timrelate_Status[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue *int64
-}
-
-func (t *timrelate_Status[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *timrelate_Status[T]) Value() any {
-	return t.fieldValue
-}
-
 type Timrelate struct {
 	gdao.Table[Timrelate]
 
-	Id		*timrelate_Id[Timrelate]
-	Uuid		*timrelate_Uuid[Timrelate]
-	Status		*timrelate_Status[Timrelate]
+	ID      *base.Field[Timrelate]
+	UUID      *base.Field[Timrelate]
+	STATUS      *base.Field[Timrelate]
+	TIMESERIES      *base.Field[Timrelate]
+	_ID      *int64
+	_UUID      *int64
+	_STATUS      *int64
+	_TIMESERIES      *int64
 }
 
+var _Timrelate_ID = &base.Field[Timrelate]{"id"}
+var _Timrelate_UUID = &base.Field[Timrelate]{"uuid"}
+var _Timrelate_STATUS = &base.Field[Timrelate]{"status"}
+var _Timrelate_TIMESERIES = &base.Field[Timrelate]{"timeseries"}
+
 func (u *Timrelate) GetId() (_r int64){
-	if u.Id.fieldValue != nil {
-		_r = *u.Id.fieldValue
+	if u._ID != nil {
+		_r = *u._ID
 	}
 	return
 }
 
 func (u *Timrelate) SetId(arg int64) *Timrelate{
-	u.Put0(u.Id.fieldName, arg)
-	u.Id.fieldValue = &arg
+	u.Put0(u.ID.FieldName, arg)
+	u._ID = &arg
 	return u
 }
 
 func (u *Timrelate) GetUuid() (_r int64){
-	if u.Uuid.fieldValue != nil {
-		_r = *u.Uuid.fieldValue
+	if u._UUID != nil {
+		_r = *u._UUID
 	}
 	return
 }
 
 func (u *Timrelate) SetUuid(arg int64) *Timrelate{
-	u.Put0(u.Uuid.fieldName, arg)
-	u.Uuid.fieldValue = &arg
+	u.Put0(u.UUID.FieldName, arg)
+	u._UUID = &arg
 	return u
 }
 
 func (u *Timrelate) GetStatus() (_r int64){
-	if u.Status.fieldValue != nil {
-		_r = *u.Status.fieldValue
+	if u._STATUS != nil {
+		_r = *u._STATUS
 	}
 	return
 }
 
 func (u *Timrelate) SetStatus(arg int64) *Timrelate{
-	u.Put0(u.Status.fieldName, arg)
-	u.Status.fieldValue = &arg
+	u.Put0(u.STATUS.FieldName, arg)
+	u._STATUS = &arg
+	return u
+}
+
+func (u *Timrelate) GetTimeseries() (_r int64){
+	if u._TIMESERIES != nil {
+		_r = *u._TIMESERIES
+	}
+	return
+}
+
+func (u *Timrelate) SetTimeseries(arg int64) *Timrelate{
+	u.Put0(u.TIMESERIES.FieldName, arg)
+	u._TIMESERIES = &arg
 	return u
 }
 
@@ -116,42 +97,42 @@ func (u *Timrelate) Scan(fieldname string, value any) {
 		u.SetUuid(base.AsInt64(value))
 	case "status":
 		u.SetStatus(base.AsInt64(value))
+	case "timeseries":
+		u.SetTimeseries(base.AsInt64(value))
 	}
 }
 
 func (t *Timrelate) ToGdao() {
-	_t := NewTimrelate()
-	*t = *_t
+	t.init("timrelate")
 }
 
 func (t *Timrelate) Copy(h *Timrelate) *Timrelate{
 	t.SetId(h.GetId())
 	t.SetUuid(h.GetUuid())
 	t.SetStatus(h.GetStatus())
+	t.SetTimeseries(h.GetTimeseries())
 	return t
 }
 
 func (t *Timrelate) String() string {
-	return fmt.Sprint("Id:",t.GetId(), ",","Uuid:",t.GetUuid(), ",","Status:",t.GetStatus())
+	return fmt.Sprint("Id:",t.GetId(), ",","Uuid:",t.GetUuid(), ",","Status:",t.GetStatus(), ",","Timeseries:",t.GetTimeseries())
+}
+
+func (t *Timrelate)init(tablename string) {
+	t.ID = _Timrelate_ID
+	t.UUID = _Timrelate_UUID
+	t.STATUS = _Timrelate_STATUS
+	t.TIMESERIES = _Timrelate_TIMESERIES
+	t.Init(tablename, []base.Column[Timrelate]{t.ID,t.UUID,t.STATUS,t.TIMESERIES})
 }
 
 func NewTimrelate(tablename ...string) (_r *Timrelate) {
-
-	id := &timrelate_Id[Timrelate]{fieldName: "id"}
-	id.Field.FieldName = "id"
-
-	uuid := &timrelate_Uuid[Timrelate]{fieldName: "uuid"}
-	uuid.Field.FieldName = "uuid"
-
-	status := &timrelate_Status[Timrelate]{fieldName: "status"}
-	status.Field.FieldName = "status"
-
-	_r = &Timrelate{Id:id,Uuid:uuid,Status:status}
+	_r = &Timrelate{}
 	s := "timrelate"
 	if len(tablename) > 0 && tablename[0] != "" {
 		s = tablename[0]
 	}
-	_r.Init(s, []base.Column[Timrelate]{id,uuid,status})
+	_r.init(s)
 	return
 }
 
@@ -160,6 +141,7 @@ func (t *Timrelate) Encode() ([]byte, error) {
 	m["id"] = t.GetId()
 	m["uuid"] = t.GetUuid()
 	m["status"] = t.GetStatus()
+	m["timeseries"] = t.GetTimeseries()
 	return t.Table.Encode(m)
 }
 
