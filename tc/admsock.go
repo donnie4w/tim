821a2fc6090feaccf81c3aa2067af7ee97d350a4
donnie4w <donnie4w@gmail.com>
2025-01-13 20:08:47 +0800

    Update admsock.go

--- tc/admsock.go
+++ tc/admsock.go
@@ -4,11 +4,11 @@
 // license that can be found in the LICENSE file.
 //
 // github.com/donnie4w/tim
-//
 
 package tc
 
 import (
+	"github.com/donnie4w/tim/errs"
 	"sync/atomic"
 	"time"
 
@@ -25,13 +25,13 @@ import (
 var numLock = NewNumLock(64)
 var await = NewAwait[int8](1 << 10)
 
-var admwsware = &_wsWare{wsmap: NewMapL[int64, *WsSock]()}
+var admwsware = &wsware{wsmap: NewMapL[int64, *WsSock]()}
 
-type _wsWare struct {
+type wsware struct {
 	wsmap *MapL[int64, *WsSock]
 }
 
-func (t *_wsWare) Addws(ws *tlnet.Websocket) {
+func (t *wsware) Addws(ws *tlnet.Websocket) {
 	numLock.Lock(ws.Id)
 	defer numLock.Unlock(ws.Id)
 	if t.wsmap.Has(ws.Id) {
@@ -41,7 +41,7 @@ func (t *_wsWare) Addws(ws *tlnet.Websocket) {
 	t.wsmap.Put(ws.Id, wss)
 }
 
-func (t *_wsWare) SendWs(id int64, ts thrift.TStruct, tt sys.TIMTYPE) (_r bool) {
+func (t *wsware) SendWs(id int64, ts thrift.TStruct, tt sys.TIMTYPE) (_r bool) {
 	if wss, ok := t.wsmap.Get(id); ok {
 		if err := wss.send(ts, tt, false); err == nil {
 			_r = ok
@@ -50,7 +50,7 @@ func (t *_wsWare) SendWs(id int64, ts thrift.TStruct, tt sys.TIMTYPE) (_r bool) 
 	return
 }
 
-func (t *_wsWare) SendWsWithAck(id int64, ts thrift.TStruct, tt sys.TIMTYPE) (_r bool) {
+func (t *wsware) SendWsWithAck(id int64, ts thrift.TStruct, tt sys.TIMTYPE) (_r bool) {
 	if wss, ok := t.wsmap.Get(id); ok {
 		if err := wss.send(ts, tt, true); err == nil {
 			_r = ok
@@ -59,37 +59,37 @@ func (t *_wsWare) SendWsWithAck(id int64, ts thrift.TStruct, tt sys.TIMTYPE) (_r
 	return
 }
 
-func (t *_wsWare) Ping(id int64) {
+func (t *wsware) Ping(id int64) {
 	if wss, ok := t.wsmap.Get(id); ok {
 		wss.send(nil, sys.ADMPING, false)
 	}
 }
 
-func (t *_wsWare) Get(ws *tlnet.Websocket) (*WsSock, bool) {
+func (t *wsware) Get(ws *tlnet.Websocket) (*WsSock, bool) {
 	return t.wsmap.Get(ws.Id)
 }
 
-func (t *_wsWare) hasws(ws *tlnet.Websocket) bool {
+func (t *wsware) hasws(ws *tlnet.Websocket) bool {
 	return t.wsmap.Has(ws.Id)
 }
 
-func (t *_wsWare) wsById(id int64) (*tlnet.Websocket, bool) {
+func (t *wsware) wsById(id int64) (*tlnet.Websocket, bool) {
 	if ws, b := t.wsmap.Get(id); b {
 		return ws.ws, b
 	}
 	return nil, false
 }
 
-func (t *_wsWare) wsLen() int64 {
+func (t *wsware) wsLen() int64 {
 	return t.wsmap.Len()
 }
 
-func (t *_wsWare) delws(ws *tlnet.Websocket) {
+func (t *wsware) delws(ws *tlnet.Websocket) {
 	defer util.Recover()
 	t.delId(ws.Id)
 }
 
-func (t *_wsWare) delId(id int64) {
+func (t *wsware) delId(id int64) {
 	if sk, ok := t.wsmap.Get(id); ok {
 		sk.ws.Close()
 		t.wsmap.Del(id)
@@ -113,7 +113,16 @@ func (t *WsSock) _send(buf *Buffer) (err error) {
 var seq int32 = 0
 
 func (t *WsSock) send(ts thrift.TStruct, tt sys.TIMTYPE, sync bool) (err error) {
-	buf := NewBuffer()
+	length := 1
+	if sync {
+		length = 5
+	}
+	var bs []byte
+	if ts != nil {
+		bs = TEncode(ts)
+		length += len(bs)
+	}
+	buf := NewBufferWithCapacity(length)
 	sendId := atomic.AddInt32(&seq, 1)
 	var ch chan int8
 	if sync {
@@ -123,8 +132,11 @@ func (t *WsSock) send(ts thrift.TStruct, tt sys.TIMTYPE, sync bool) (err error) 
 	} else {
 		buf.WriteByte(byte(tt))
 	}
-	if ts != nil {
-		buf.Write(TEncode(ts))
+	//if ts != nil {
+	//	buf.Write(TEncode(ts))
+	//}
+	if len(bs) > 0 {
+		buf.Write(bs)
 	}
 	if err = t._send(buf); err == nil && sync {
 		i := 0
@@ -135,28 +147,28 @@ func (t *WsSock) send(ts thrift.TStruct, tt sys.TIMTYPE, sync bool) (err error) 
 				err = nil
 				goto END
 			case <-time.After(time.Second):
-				err = sys.ERR_OVERTIME.Error()
+				err = errs.ERR_OVERTIME.Error()
 			}
 		}
 		if t.ws.Error != nil || err != nil {
-			err = sys.ERR_OVERTIME.Error()
+			err = errs.ERR_OVERTIME.Error()
 		}
 	}
 END:
 	return
 }
 
-func (t *WsSock) sendBigData(data []byte, tt sys.TIMTYPE) (err error) {
-	buf := NewBuffer()
-	buf.WriteByte(byte(tt))
-	buf.Write(data)
-	return t._send(buf)
-}
+//func (t *WsSock) sendBigData(data []byte, tt sys.TIMTYPE) (err error) {
+//	buf := NewBuffer()
+//	buf.WriteByte(byte(tt))
+//	buf.Write(data)
+//	return t._send(buf)
+//}
 
 func (t *WsSock) close() {
 	admwsware.delws(t.ws)
 }
 
 func awaitEnd(bs []byte) {
-	await.DelAndClose(int64(BytesToInt32(bs)))
+	await.Close(int64(BytesToInt32(bs)))
 }
