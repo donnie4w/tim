ca46f245ca721dc4c748abde1ee7a83046f4e85a
donnie4w <donnie4w@gmail.com>
2025-01-14 14:18:26 +0800

    Update log.go

--- sys/log.go
+++ sys/log.go
@@ -4,40 +4,19 @@
 // license that can be found in the LICENSE file.
 //
 // github.com/donnie4w/tim
-//
 
 package sys
 
 import (
-	"fmt"
-
-	"github.com/donnie4w/simplelog/logging"
+	"github.com/donnie4w/tim/log"
 )
-
-var log = logging.NewLogger().SetFormat(logging.FORMAT_DATE | logging.FORMAT_TIME).SetLevel(logging.LEVEL_INFO)
-
-func FmtLog(v ...any) {
-	info := fmt.Sprint(v...)
-	a, b := "", ""
-	ll := 80
-	if ll >= len(info) {
-		for i := 0; i < (ll-len(info))/2; i++ {
-			a = a + "="
-		}
-		b = a
-		if ll > len(info)+len(a)*2 {
-			b = a + "="
-		}
-	}
-	log.Info(a, info, b)
-}
 
 func blankLine() {
 	log.Write([]byte("\n"))
 }
 
-func timlogo(){
-	_r :=`
+func timlogo() {
+	_r := `
 	=================================================================================
 	===========        ===============   ===   ======       ======        ===========
 	===========              ===               === ===     === ===        ===========
