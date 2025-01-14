eb93a6ec3fdd02beb9b55662bdbf73ff5e20a208
donnie4w <donnie4w@gmail.com>
2025-01-14 14:18:14 +0800

    Update var.go

--- sys/var.go
+++ sys/var.go
@@ -4,56 +4,21 @@
 // license that can be found in the LICENSE file.
 //
 // github.com/donnie4w/tim
-//
 
 package sys
 
 import (
 	"fmt"
+	"github.com/donnie4w/gofer/hashmap"
+	"github.com/donnie4w/gofer/util"
+	"github.com/donnie4w/tim/stub"
 	"time"
-
-	. "github.com/donnie4w/gofer/hashmap"
-	. "github.com/donnie4w/gofer/util"
 )
 
-const VERSION = "2.0.0"
-
-const (
-	MB = 1 << 20
-	GB = 1 << 30
-)
-
-const (
-	TRANS_SOURCE     int8 = 1
-	TRANS_CONSISHASH int8 = 2
-	TRANS_STAFF      int8 = 3
-	TRANS_GOAL       int8 = 4
-)
-
-const (
-	ORDER_INOF      int8 = 1
-	ORDER_REVOKE    int8 = 2
-	ORDER_BURN      int8 = 3
-	ORDER_BUSINESS  int8 = 4
-	ORDER_STREAM    int8 = 5
-	ORDER_BIGSTRING int8 = 6
-	ORDER_BIGBINARY int8 = 7
-	ORDER_RESERVED  int8 = 30
-)
-
-const (
-	CB_MESSAGE  int8 = 1
-	CB_PRESENCE int8 = 2
-)
-
-const (
-	SOURCE_OS   int8 = 1
-	SOURCE_USER int8 = 2
-	SOURCE_ROOM int8 = 3
-)
+const VERSION = "2.0.2"
 
 var (
-	Service   = NewSortMap[int, Server]()
+	Service   = hashmap.NewTreeMap[int, Server](5)
 	Tim       Server
 	STARTTIME = time.Now()
 	UUID      int64
@@ -62,17 +27,17 @@ var (
 	ORIGIN    string
 	DEBUGADDR string
 
-	LA                   bool
-	TIMJSON              string
-	KEYSTORE             string
-	SEP_BIN              = byte(131)
-	SEP_STR              = "|"
-	Stat                 istat
-	Conf                 *ConfBean
-	Bind                 string
-	DBtype               byte
+	LA       bool
+	TIMJSON  string
+	KEYSTORE string
+	SEP_BIN  = byte(131)
+	SEP_STR  = "|"
+	Stat     istat
+	Conf     *stub.ConfBean
+	Bind     string
+
 	DefaultAccount       = [2]string{"admin", "123"}
-	MaskSeed             = Int64ToBytes(int64(1 << 60))
+	MaskSeed             = util.Int64ToBytes(int64(1 << 60))
 	ADMADDR              = ""
 	WEBADMINADDR         = fmt.Sprint(6 << 10)
 	CSADDR               = fmt.Sprint(7 << 10)
@@ -86,13 +51,14 @@ var (
 	DeviceTypeLimit      = 1
 	MaxBackup            = 3
 	NodeMaxlength        = 64
-	ForBitIfaceMap       *Map[TIMTYPE, int8]
-	OpenSSL              = &openssl{}
+	BlockApiMap          *hashmap.Map[TIMTYPE, int8]
+	OpenSSL              = &stub.Openssl{}
 	defaultPwd           = "tim20171212"
 	defaultAesencryptkey = "ie8*&(I984){bW{@a@#￥%H'"
 	defaultConnectLimit  = int64(1 << 24)
 	defaultMemlimit      = 1 << 11
 	defaultsyssalt       = "#@*=+-<>?:|$&()%$#{]aQkLIPM79643028U'TRKF_}"
-	defaultTasks         = 1 << 8
+	defaultLimitRate     = int64(1 << 8)
 	defaultConf          = ""
+	defaultTTL           = uint64(24 * 60 * 60) // 1 day
 )
