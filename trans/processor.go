c3682eb5e59a20333dbeb6051bd94d59dab70c89
donnie4w <donnie4w@gmail.com>
2025-01-17 12:13:32 +0800

    Update processor.go

--- trans/processor.go
+++ trans/processor.go
@@ -1,6 +1,7 @@
 package trans
 
 import (
+	"github.com/donnie4w/tim/cache"
 	"github.com/donnie4w/tim/stub"
 	"github.com/donnie4w/tim/sys"
 	"github.com/donnie4w/tim/vgate"
@@ -21,45 +22,51 @@ func (p *processor) addNoAck() int32 {
 }
 
 func (p *processor) TimMessage(syncId int64, tm *stub.TimMessage) (err error) {
-	if len(tm.GetToList()) > 0 {
-		if sys.TimMessageProcessor(tm, sys.TRANS_GOAL) == nil && syncId != 0 {
-			p.tran.TimAck(syncId)
-		}
+	if len(tm.GetToList()) > 0 || tm.GetToTid() != nil {
+		go sys.TimMessageProcessor(tm, sys.TRANS_GOAL)
+	}
+	if syncId != 0 {
+		p.tran.TimAck(syncId)
 	}
 	return nil
 }
 
 func (p *processor) TimPresence(syncId int64, tp *stub.TimPresence) (err error) {
-	if len(tp.GetToList()) > 0 {
-		if sys.TimPresenceProcessor(tp, sys.TRANS_GOAL) == nil && syncId != 0 {
-			p.tran.TimAck(syncId)
+	if len(tp.GetToList()) > 0 || tp.GetToTid() != nil {
+		if tp.GetOffline() && tp.FromTid != nil {
+			cache.AccountCache.Del(tp.FromTid.GetNode())
 		}
+		go sys.TimPresenceProcessor(tp, sys.TRANS_GOAL)
+	}
+	if syncId != 0 {
+		p.tran.TimAck(syncId)
 	}
 	return nil
 }
 
 func (p *processor) TimStream(syncId int64, vb *stub.VBean) (err error) {
-	if sys.TimSteamProcessor(vb, sys.TRANS_GOAL) == nil && syncId != 0 {
+	go sys.TimSteamProcessor(vb, sys.TRANS_GOAL)
+	if syncId != 0 {
 		p.tran.TimAck(syncId)
 	}
 	return nil
 }
 
 func (p *processor) TimCsVBean(syncId int64, vb *stub.CsVrBean) (err error) {
-	switch sys.TIMTYPE(vb.GetVbean().GetDtype()) {
-	case sys.VROOM_NEW:
-	case sys.VROOM_REMOVE:
-	case sys.VROOM_SUB:
-		if vb.GetVbean().GetRnode() != "" && vb.GetSrcuuid() != 0 && vb.GetSrcuuid() != sys.UUID {
-			vgate.VGate.Sub(vb.GetVbean().GetVnode(), vb.GetSrcuuid(), 0)
+	go func() {
+		switch sys.TIMTYPE(vb.GetVbean().GetRtype()) {
+		case sys.VROOM_SUB:
+			if vb.GetVbean().GetRnode() != "" && vb.GetSrcuuid() != 0 && vb.GetSrcuuid() != sys.UUID {
+				vgate.VGate.Sub(vb.GetVbean().GetVnode(), vb.GetSrcuuid(), 0)
+			}
+		case sys.VROOM_UNSUB:
+			if vb.GetSrcuuid() != 0 && vb.GetSrcuuid() != sys.UUID {
+				vgate.VGate.UnSubWithUUID(vb.GetVbean().GetVnode(), vb.GetSrcuuid())
+			}
+		case sys.VROOM_MESSAGE:
+			sys.TimSteamProcessor(vb.GetVbean(), sys.TRANS_GOAL)
 		}
-	case sys.VROOM_UNSUB:
-		if vb.GetSrcuuid() != 0 && vb.GetSrcuuid() != sys.UUID {
-			vgate.VGate.UnSubWithUUID(vb.GetVbean().GetVnode(), vb.GetSrcuuid())
-		}
-	case sys.VROOM_MESSAGE:
-		sys.TimSteamProcessor(vb.GetVbean(), sys.TRANS_GOAL)
-	}
+	}()
 	if syncId != 0 {
 		p.tran.TimAck(syncId)
 	}
@@ -69,6 +76,18 @@ func (p *processor) TimCsVBean(syncId int64, vb *stub.CsVrBean) (err error) {
 func (p *processor) TimAck(syncId int64) (err error) {
 	p.tw.dataWait.Close(syncId)
 	return nil
+}
+
+func (p *processor) TimCsDevice(syncId int64, cd *stub.CsDevice) (err error) {
+	bs := sys.DeviceTypeList(cd.GetNode())
+	cd.TypeList = bs
+	p.tran.TimCsDeviceAck(syncId, cd)
+	return
+}
+
+func (p *processor) TimCsDeviceAck(syncId int64, cd *stub.CsDevice) (err error) {
+	p.tw.dataWait.CloseAndPut(syncId, cd)
+	return
 }
 
 func (p *processor) Id() int64 {
