47d131c7befe6cf277f8f2f9f5f7421e801babca
donnie4w <donnie4w@gmail.com>
2025-01-20 10:51:18 +0800

    Update var.go

--- sys/var.go
+++ sys/var.go
@@ -8,7 +8,6 @@
 package sys
 
 import (
-	"fmt"
 	"github.com/donnie4w/gofer/hashmap"
 	"github.com/donnie4w/gofer/util"
 	"github.com/donnie4w/tim/stub"
@@ -18,47 +17,44 @@ import (
 const VERSION = "2.0.2"
 
 var (
-	Service   = hashmap.NewTreeMap[int, Server](5)
-	Tim       Server
-	STARTTIME = time.Now()
-	UUID      int64
-	LOGDEBUG  bool
-	GOGC      int
-	ORIGIN    string
-	DEBUGADDR string
+	STARTTIME              = time.Now()
+	DefaultAccount         = [2]string{"admin", "123"}
+	MaskSeed               = util.Int64ToBytes(int64(1 << 60))
+	UUIDCSTIME             = 60 //60 second
+	ConnectTimeout         = 10 * time.Second
+	WaitTimeout            = 10 * time.Second
+	MaxTransLength         = 10 * MB
+	DeviceLimit            = 1
+	DeviceTypeLimit        = 1
+	MaxBackup              = 3
+	NodeMaxlength          = 64
+	BlockApiMap            *hashmap.Map[TIMTYPE, int8]
+	OpenSSL                = &stub.Openssl{}
+	UUID                   int64
+	LOGDEBUG               bool
+	GOGC                   int
+	ORIGIN                 string
+	TIMJSON                string
+	KEYSTORE               string
+	SEP_BIN                = byte(131)
+	SEP_STR                = "|"
+	Stat                   istat
+	Conf                   *stub.ConfBean
+	service                = hashmap.NewTreeMap[int, Server](5)
+	defaultPingTimeot      = int64(600) // 600 second
+	defaultPwd             = "tim20171212"
+	defaultAesencryptkey   = "ie8*&(I984){bW{@a@#￥%H'"
+	defaultConnectLimit    = int64(1 << 24)
+	defaultMemlimit        = 1 << 10
+	defaultSalt            = "#@*=+-<>?:|$&()%$#{]aQkLIPM79643028U'TRKF_}"
+	defaultLimitRate       = int64(1 << 8)
+	defaultConf            = ""
+	defaultTTL             = uint64(24 * 60 * 60) // 1 day
+	defaultTokenTimeout    = 10 * time.Second.Nanoseconds()
+	defaultCacheAuthExpire = int64(300) //300 Second
+)
 
-	LA       bool
-	TIMJSON  string
-	KEYSTORE string
-	SEP_BIN  = byte(131)
-	SEP_STR  = "|"
-	Stat     istat
-	Conf     *stub.ConfBean
-	Bind     string
-
-	DefaultAccount       = [2]string{"admin", "123"}
-	MaskSeed             = util.Int64ToBytes(int64(1 << 60))
-	ADMADDR              = ""
-	WEBADMINADDR         = fmt.Sprint(6 << 10)
-	CSADDR               = fmt.Sprint(7 << 10)
-	IMADDR               = 5 << 10
-	InaccurateTime       = time.Now().UnixNano()
-	PINGTO               = int64(500)
-	ConnectTimeout       = 10 * time.Second
-	WaitTimeout          = 10 * time.Second
-	MaxTransLength       = 10 * MB
-	DeviceLimit          = 1
-	DeviceTypeLimit      = 1
-	MaxBackup            = 3
-	NodeMaxlength        = 64
-	BlockApiMap          *hashmap.Map[TIMTYPE, int8]
-	OpenSSL              = &stub.Openssl{}
-	defaultPwd           = "tim20171212"
-	defaultAesencryptkey = "ie8*&(I984){bW{@a@#￥%H'"
-	defaultConnectLimit  = int64(1 << 24)
-	defaultMemlimit      = 1 << 11
-	defaultsyssalt       = "#@*=+-<>?:|$&()%$#{]aQkLIPM79643028U'TRKF_}"
-	defaultLimitRate     = int64(1 << 8)
-	defaultConf          = ""
-	defaultTTL           = uint64(24 * 60 * 60) // 1 day
+var (
+	LA   bool
+	Bind string
 )
