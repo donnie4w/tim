34bc476c6e6e67feb602ceaf7b568007d0e2a3ca
donnie4w <donnie4w@gmail.com>
2025-04-23 19:57:28 +0800

    Update gdao.go

--- data/gdao.go
+++ data/gdao.go
@@ -9,11 +9,9 @@ package data
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/donnie4w/gdao"
 	"github.com/donnie4w/gdao/base"
-	"github.com/donnie4w/gofer/util"
 	"github.com/donnie4w/tim/stub"
 	"github.com/donnie4w/tim/sys"
 	_ "github.com/go-sql-driver/mysql"
@@ -21,45 +19,10 @@ import (
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 	"sort"
-	"strings"
 )
 
-var (
-	m_user   = fmt.Sprint(util.UUID64())
-	m_pwd    = fmt.Sprint(util.UUID64())
-	m_host   = fmt.Sprint(util.UUID64())
-	m_port   = fmt.Sprint(util.UUID64())
-	m_dbname = fmt.Sprint(util.UUID64())
-
-	mysqlMod      = fmt.Sprint(m_user, ":", m_pwd, "@tcp(", m_host, ":", m_port, ")/", m_dbname)
-	postgreSQLMod = fmt.Sprint("host=", m_host, " port=", m_port, " user=", m_user, " password=", m_pwd, " dbname=", m_dbname, " sslmode=disable")
-	oracleMod     = fmt.Sprint(`user="`, m_user, `" password="`, m_pwd, `" connectstring="`, m_host, ":", m_port, "/", m_dbname, `"`)
-	sqlserverMod  = fmt.Sprint("server=", m_host, ";port=", m_port, ";userid=", m_user, ";password=", m_pwd, ";database=", m_dbname)
-)
-
-func parseConnection(s string, dbmode string) string {
-	if ss := strings.Split(strings.TrimSpace(s), " "); len(ss) == 5 {
-		for _, sv := range ss {
-			idx := strings.Index(strings.TrimSpace(sv), "=")
-			k, v := sv[:idx], sv[idx+1:]
-			if k == "host" {
-				dbmode = strings.ReplaceAll(dbmode, m_host, v)
-			} else if k == "port" {
-				dbmode = strings.ReplaceAll(dbmode, m_port, v)
-			} else if k == "user" {
-				dbmode = strings.ReplaceAll(dbmode, m_user, v)
-			} else if k == "password" {
-				dbmode = strings.ReplaceAll(dbmode, m_pwd, v)
-			} else if k == "dbname" {
-				dbmode = strings.ReplaceAll(dbmode, m_dbname, v)
-			}
-		}
-	}
-	return dbmode
-}
-
-func getDBhandle(driverName, dataSourceName string) (dbhandle base.DBhandle, err error) {
-	if db, er := sql.Open(driverName, dataSourceName); er == nil {
+func getDBhandle(driverName, dsn string) (dbhandle base.DBhandle, err error) {
+	if db, er := sql.Open(driverName, dsn); er == nil {
 		if err = db.Ping(); err == nil {
 			dbhandle = gdao.NewDBHandle(db, getDBtype(driverName))
 		}
@@ -96,12 +59,13 @@ type sourceHandle struct {
 	dbsource []*sqlsource
 }
 
-func (sh *sourceHandle) Add(driverName, dataSourceName string, extent int) error {
+func (sh *sourceHandle) AddConnect(dbType base.DBType, connect *stub.Connect, extent int) error {
 	if extent == 0 {
 		extent = sys.MB
 	}
-	if dbhandle, err := getDBhandle(driverName, dataSourceName); err == nil {
-		initLocalDB(dbhandle, driverName)
+	driver, dsn := connect.DSN(dbType)
+	if dbhandle, err := getDBhandle(driver, dsn); err == nil {
+		initLocalDB(dbhandle, driver)
 		sh.dbsource = append(sh.dbsource, &sqlsource{extent: extent, dbhandle: dbhandle})
 	} else {
 		return err
@@ -111,22 +75,33 @@ func (sh *sourceHandle) Add(driverName, dataSourceName string, extent int) error
 }
 
 func (sh *sourceHandle) AddInlineDB(v *stub.InlineDB) error {
-	if v.Tim_sqlite_connection != "" {
-		return sh.Add(Driver_Sqlite, v.Tim_sqlite_connection, v.ExtentMax)
-	} else if v.Tim_postgresql_connection != "" {
-		return sh.Add(Driver_Postgres, v.Tim_postgresql_connection, v.ExtentMax)
-	} else if v.Tim_mysql_connection != "" {
-		return sh.Add(Driver_Mysql, v.Tim_mysql_connection, v.ExtentMax)
-	} else if v.Tim_sqlserver_connection != "" {
-		return sh.Add(Driver_Sqlserver, v.Tim_sqlserver_connection, v.ExtentMax)
-	} else if v.Tim_postgresql_connection_mod != "" {
-		return sh.Add(Driver_Postgres, v.Tim_postgresql_connection, v.ExtentMax)
-	} else if v.Tim_mysql_connection_mod != "" {
-		return sh.Add(Driver_Mysql, v.Tim_mysql_connection, v.ExtentMax)
-	} else if v.Tim_sqlserver_connection_mod != "" {
-		return sh.Add(Driver_Sqlserver, v.Tim_sqlserver_connection, v.ExtentMax)
+	if v.SQLITE != nil {
+		return sh.AddConnect(gdao.SQLITE, v.SQLITE, v.ExtentMax)
+	} else if v.POSTGRESQL != nil {
+		return sh.AddConnect(gdao.POSTGRESQL, v.POSTGRESQL, v.ExtentMax)
+	} else if v.GREENPLUM != nil {
+		return sh.AddConnect(gdao.GREENPLUM, v.GREENPLUM, v.ExtentMax)
+	} else if v.OPENGAUSS != nil {
+		return sh.AddConnect(gdao.OPENGAUSS, v.OPENGAUSS, v.ExtentMax)
+	} else if v.COCKROACHDB != nil {
+		return sh.AddConnect(gdao.COCKROACHDB, v.COCKROACHDB, v.ExtentMax)
+	} else if v.ENTERPRISEDB != nil {
+		return sh.AddConnect(gdao.ENTERPRISEDB, v.ENTERPRISEDB, v.ExtentMax)
+	} else if v.MYSQL != nil {
+		return sh.AddConnect(gdao.MYSQL, v.MYSQL, v.ExtentMax)
+	} else if v.MARIADB != nil {
+		return sh.AddConnect(gdao.MARIADB, v.MARIADB, v.ExtentMax)
+	} else if v.OCEANBASE != nil {
+		return sh.AddConnect(gdao.OCEANBASE, v.OCEANBASE, v.ExtentMax)
+	} else if v.TIDB != nil {
+		return sh.AddConnect(gdao.TIDB, v.TIDB, v.ExtentMax)
+	} else if v.SQLSERVER != nil {
+		return sh.AddConnect(gdao.SQLSERVER, v.SQLSERVER, v.ExtentMax)
+	} else if v.ORACLE != nil {
+		return sh.AddConnect(gdao.ORACLE, v.ORACLE, v.ExtentMax)
+	} else {
+		return sh.AddConnect(gdao.SQLITE, &stub.Connect{DBname: localdb}, v.ExtentMax)
 	}
-	return nil
 }
 
 func (sh *sourceHandle) GetDBHandle(tid uint64) base.DBhandle {
@@ -148,21 +123,4 @@ func (sh *sourceHandle) FirstDBHandle() base.DBhandle {
 		return sh.dbsource[0].dbhandle
 	}
 	return nil
-}
-
-func lastInsertId(dbtype base.DBType, tx base.Transaction) (int64, bool) {
-	var s string
-	switch dbtype {
-	case gdao.SQLITE:
-		s = "SELECT LAST_INSERT_ROWID()"
-	case gdao.MYSQL, gdao.MARIADB, gdao.TIDB, gdao.OCEANBASE:
-		s = "SELECT LAST_INSERT_ID()"
-	case gdao.SQLSERVER:
-		s = "SELECT SCOPE_IDENTITY()"
-	case gdao.POSTGRESQL, gdao.GREENPLUM, gdao.OPENGAUSS:
-		return 0, true
-	default:
-		return 0, false
-	}
-	return tx.ExecuteQueryBean(s).ToInt64(), false
 }
