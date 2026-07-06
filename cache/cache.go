edaa9cdea2e1c6e3f6e2a439ef9b2b504e2f3938
donnie4w <donnie4w@gmail.com>
2026-07-06 20:55:44 +0800

    Update cache.go

--- cache/cache.go
+++ cache/cache.go
@@ -10,18 +10,41 @@ package cache
 import (
 	gocache "github.com/donnie4w/gofer/cache"
 	"github.com/donnie4w/gofer/hashmap"
+	"github.com/donnie4w/tim/sys"
 )
 
-var TokenCache = newTokenPool()
+var (
+	TokenCache     *tokenPool
+	AuthCache      *auth
+	AccountCache   *accountPool
+	BlockUserCache *blockPool
+	CsAccessCache  *csAccessPool
+	TokenUsedCache *gocache.BloomFilter
+	VnodeCache     *hashmap.LimitFifoMap[uint64, int64]
+)
 
-var AuthCache = newAuth()
+func init() {
+	sys.Service(sys.INIT_CACHE, serv(1))
+}
 
-var AccountCache = newAccountPool()
+type serv byte
 
-var TokenUsedCache = gocache.NewBloomFilter(1<<19, 0.01)
+func (serv) Serve() error {
+	if sys.Conf.Memlimit >= 1<<10 {
+		TokenUsedCache = gocache.NewBloomFilter(1<<19, 0.01)
+		VnodeCache = hashmap.NewLimitFifoMap[uint64, int64](1 << 18)
+	} else {
+		TokenUsedCache = gocache.NewBloomFilter(1<<13, 0.01)
+		VnodeCache = hashmap.NewLimitFifoMap[uint64, int64](1 << 13)
+	}
+	TokenCache = newTokenPool()
+	AuthCache = newAuth()
+	AccountCache = newAccountPool()
+	BlockUserCache = newBlockPool()
+	CsAccessCache = newCsAccessPool()
+	return nil
+}
 
-var BlockUserCache = newBlockPool()
-
-var VnodeCache = hashmap.NewLimitHashMap[uint64, int64](1 << 18)
-
-var CsAccessCache = newCsAccessPool()
+func (serv) Close() error {
+	return nil
+}
